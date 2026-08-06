185570e76be789bcc9ce48d13f4d4d4db4459dee
nirvanajoshi <damodarj36@gmail.com>
2026-08-06 23:30:40 +0545

    Model Profile

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    SEMESTER_CHOICES = [
+        ('1', '1st Semester'),
+        ('2', '2nd Semester'),
+        ('3', '3rd Semester'),
+        ('4', '4th Semester'),
+        ('5', '5th Semester'),
+        ('6', '6th Semester'),
+        ('7', '7th Semester'),
+        ('8', '8th Semester'),
+    ]
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    college = models.CharField(max_length=100, blank=True)
+    semester = models.PositiveBigIntegerField(blank=True,null=True, choices=SEMESTER_CHOICES)
+    daily_study_goal = models.IntegerField(default=60,help_text="Daily study goal in minutes")
+    bio = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+        ordering = ["user__username"]
+    
+    def __str__(self):
+        return self.user.username
+    
