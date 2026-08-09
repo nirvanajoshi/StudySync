1509dadd69c55357ae780a09768493a53030d594
nirvanajoshi <damodarj36@gmail.com>
2026-08-09 18:55:19 +0545

    Exam  Urls

--- apps/exams/urls.py
+++ apps/exams/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from . import views
 
 app_name = "exams"
 
 urlpatterns = [
-
+    path("", views.exam_list, name="exam_list"),
+    path("create/", views.exam_create, name="exam_create"),
+    path("<int:pk>/", views.exam_detail, name="exam_detail"),
+    path("<int:pk>/edit/", views.exam_edit, name="exam_edit"),
+    path("<int:pk>/delete/", views.exam_delete, name="exam_delete"),
 ]
