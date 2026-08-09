e0af63152b1ae58e5dc92aea44802b88249ff307
nirvanajoshi <damodarj36@gmail.com>
2026-08-09 18:56:10 +0545

    Progress Urls

--- apps/progress/urls.py
+++ apps/progress/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from . import views
 
 app_name = "progress"
 
 urlpatterns = [
-
+    path("", views.progress_dashboard, name="progress_dashboard"),
+    path(
+        "subject/<int:subject_pk>/",
+        views.subject_progress,
+        name="subject_progress",
+    ),
 ]
