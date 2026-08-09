d17717da9cbcdf9fe1b8520264c1205e494af181
nirvanajoshi <damodarj36@gmail.com>
2026-08-09 18:52:45 +0545

    Config Urls

--- config/urls.py
+++ config/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("", include("apps.dashboard.urls")),
-
     path("accounts/", include("apps.accounts.urls")),
     path("subjects/", include("apps.subjects.urls")),
     path("planner/", include("apps.planner.urls")),
@@ -29,3 +31,10 @@ urlpatterns = [
     path("progress/", include("apps.progress.urls")),
     path("notes/", include("apps.notes.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
